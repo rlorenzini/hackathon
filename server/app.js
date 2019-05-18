b721d8a2b251c580158e859eb2dbcaf89ed747bc
Gregory Priznar <gregorypriznar@Gregorys-MacBook-Pro.local>
2019-05-18 14:24:32 -0500

    getData get route

--- server/app.js
+++ server/app.js
@@ -6,10 +6,14 @@ const PORT = process.env.PORT || 8080
 
 app.use(cors())
 app.use(bodyParser.json())
+models = require('/.models')
 
 app.get('/api/getData', (req, res) => {
-  res.json({message: "Eventually all the data from Database of decibel readings..."})
-})
+  models.NpReport.findAll().then(result => res.json(result))
+  console.log(result)
+  res.json({success: true, message:"Data is being populated..."})
+}).catch(error => res.json({success: false, message:"ERROR: Data could not be populated..."}))
+
 
 app.post('/api/reading', (req,res) => {
   let lat = req.body.lat
