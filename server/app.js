c7a70775a443e874101feaaa2267ab7787bd48ef
Shaun W Colley <swc7838@gmail.com>
2019-05-18 14:47:02 -0500

    resolve conflict

--- server/app.js
+++ server/app.js
@@ -6,13 +6,15 @@ const PORT = process.env.PORT || 8080
 
 app.use(cors())
 app.use(bodyParser.json())
+
 models = require('./models')
 
+
 app.get('/api/getData', (req, res) => {
-  models.NpReport.findAll().then(result => {
-    res.json({success: true, message:"Data is being populated...", result: result})
-  }).catch(error => res.json({success: false, message:"ERROR: Data could not be populated..."}))
-})
+  models.NpReport.findAll().then(result => res.json(result))
+  console.log(result)
+  res.json({success: true, message:"Data is being populated..."})
+}).catch(error => res.json({success: false, message:"ERROR: Data could not be populated..."}))
 
 
 app.post('/api/reading', (req,res) => {
